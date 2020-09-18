9a83e3e8567835503ad851ff8fc427f5f022e243
Nattida Wong <nattidac.wong@gmail.com>
2020-09-18 21:01:31 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  while i < collection.length
+    yield(collection[1])
+    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
+  end
 end
+
+my_all?([1,2,3]) {|i| i < 2}
